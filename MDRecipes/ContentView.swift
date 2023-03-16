4d972e284e836ea42770cab611b6339f8f57a2d7
nilsmango <0@project7iii.ch>
2023-03-16 20:21:07 +0100

    Initial Commit

---
+++ MDRecipes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MDRecipes
+//
+//  Created by Simon Lang on 16.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MDRecipes/MDRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDRecipesApp.swift
+//  MDRecipes
+//
+//  Created by Simon Lang on 16.03.23.
+//
+
+import SwiftUI
+
+@main
+struct MDRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
