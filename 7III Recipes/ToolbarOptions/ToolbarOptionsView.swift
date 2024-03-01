ce3082f4f72c4ca745461dcad85225d36b478976
nilsmango <simonmilagrolang@icloud.com>
2024-03-01 14:48:03 +0100

    Menu fix.

--- 7III Recipes/ToolbarOptions/ToolbarOptionsView.swift
+++ 7III Recipes/ToolbarOptions/ToolbarOptionsView.swift
@@ -18,57 +18,57 @@ struct ToolbarOptionsView: View {
     
     var body: some View {
         Menu {
-                
-                NavigationLink(destination: AboutView()) {
-                    Label("About", systemImage: "info.circle")
-                }
-                
-                NavigationLink(destination: PrivacyView()) {
-                    Label("Privacy Notice", systemImage: "doc.text.magnifyingglass")
-                }
-                
-                Button(action: {
-                    // TODO: Add donation thing
-                }) {
-                    Label("Tip us 1 USD!", systemImage: "heart")
-                }
-                
-                Divider()
+            
+            NavigationLink(destination: AboutView()) {
+                Label("About", systemImage: "info.circle")
+            }
+            
+            NavigationLink(destination: PrivacyView()) {
+                Label("Privacy Notice", systemImage: "doc.text.magnifyingglass")
+            }
+            
+            Button(action: {
+                // TODO: Add donation thing
+            }) {
+                Label("Tip us 1 USD!", systemImage: "heart")
+            }
+            
+            Divider()
             
             NavigationLink(destination: ExportView(recipesManager: fileManager)) {
                 Label("Export Recipes", systemImage: "square.and.arrow.up")
             }
-                
-                Button(action: {
-                    editViewPresented = true
-                }) { Label("Write New Recipe", systemImage: "square.and.pencil")}
-                
-                Button {
-                    importViewPresented = true
-                } label: {
-                    Label("Import Recipe from Text", systemImage: "text.viewfinder")
-                }
-                
-                Button {
-                    // import
-                    showImportSheet = true
-                } label: {
-                    Label("Import File(s)", systemImage: "square.and.arrow.down")
-                }
-                
-                
-                
-                if !fileManager.trash.isEmpty {
-                    NavigationLink(destination: TrashList(fileManager: fileManager)) {
-                        Label("Show Trash", systemImage: "trash")
-                    }
+            
+            Button(action: {
+                editViewPresented = true
+            }) { Label("Write New Recipe", systemImage: "square.and.pencil")}
+            
+            Button {
+                importViewPresented = true
+            } label: {
+                Label("Import Recipe from Text", systemImage: "text.viewfinder")
             }
-
+            
+            Button {
+                // import
+                showImportSheet = true
+            } label: {
+                Label("Import File(s)", systemImage: "square.and.arrow.down")
+            }
+            
+            if !fileManager.trash.isEmpty {
+                
+                Divider()
+                
+                NavigationLink(destination: TrashList(fileManager: fileManager)) {
+                    Label("Show Trash", systemImage: "trash")
+                }
+            }
+            
         } label: {
             Label("Options", systemImage: "ellipsis.circle")
                 .labelStyle(.iconOnly)
         }
-        
     }
 }
 
